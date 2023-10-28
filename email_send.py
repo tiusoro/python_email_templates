71299b6cabb141e5db0749b75916bc5b1f868c83
Anthony Usoro <33380362+tiusoro@users.noreply.github.com>
2023-10-28 22:36:48 +0100

    Update email_send.py

--- email_send.py
+++ email_send.py
@@ -4,7 +4,7 @@ import smtplib
 def sendEmail():
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login('tiusoro@gmail.com', 'wcih xzwb tumz dave')
+    server.login('tiusoro@gmail.com', 'wcih wcih wcih wcih')
     server.sendmail('tiusoro@gmail.com', 'pythonelearning101@gmail.com' , 'This is basically to test Email Automation')
     server.close()
     print("Email Successfully sent")
